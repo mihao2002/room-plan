548ecd6b49deaa032340cb5602914779d95fbf37
Hao Mi <mihao_2002@hotmail.com>
2025-06-20 13:16:26 -0700

    Initial Commit

---
+++ room-plan/room_planApp.swift
@@ -0,0 +1,17 @@
+//
+//  room_planApp.swift
+//  room-plan
+//
+//  Created by Hao Mi on 6/20/25.
+//
+
+import SwiftUI
+
+@main
+struct room_planApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
